b0034c4e55f798e69e03979a6b989558fd2bc6d7
Akif Ejaz <akifejaz40@gmail.com>
2022-08-24 13:29:21 +0500

    Contains the Employee DB : With CRUD Func & Classes/Enums

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # In Memory Database using C++
+
+## Usage 
+
+```
+cd In-Memory-Database-CPP
+build 
+./build/InMem_DB
+```
+
+
---
+++ build/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Akif/Younite-Internship/In Memory DB/In-Memory-Database-CPP/main.cpp" "/mnt/d/Akif/Younite-Internship/In Memory DB/In-Memory-Database-CPP/build/CMakeFiles/tests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/."
+  "../inc"
+  "../googletest/include"
+  "../googletest/googletest/include"
+  "../googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/d/Akif/Younite-Internship/In Memory DB/In-Memory-Database-CPP/build/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
